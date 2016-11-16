88f69bbc9c48708a2d17f33844864030f5fcf1a8
Dawid Ciężarkiewicz <dpc@dpc.pw>
2016-11-16 13:31:15 -0800

    Initial commit

---
+++ examples/echo.rs
@@ -0,0 +1,76 @@
+//! An echo server that just writes back everything that's written to it.
+//!
+//! If you're on unix you can test this out by in one terminal executing:
+//!
+//! ```sh
+//! $ cargo run --example echo
+//! ```
+//!
+//! and in another terminal you can run:
+//!
+//! ```sh
+//! $ nc localhost 8080
+//! ```
+//!
+//! Each line you type in to the `nc` terminal should be echo'd back to you!
+
+extern crate env_logger;
+extern crate futures;
+extern crate tokio_core;
+#[macro_use]
+extern crate tokio_fiber;
+
+use std::env;
+use std::net::SocketAddr;
+
+use futures::Future;
+use futures::stream::Stream;
+use tokio_core::net::TcpListener;
+use tokio_core::reactor::Core;
+
+use std::io::{self, Read, Write};
+
+fn main() {
+    env_logger::init().unwrap();
+    let addr = env::args().nth(1).unwrap_or("127.0.0.1:8080".to_string());
+    let addr = addr.parse::<SocketAddr>().unwrap();
+
+    // Create the event loop that will drive this server
+    let mut l = Core::new().unwrap();
+    let handle = l.handle();
+
+    // Create a TCP listener which will listen for incoming connections
+    let socket = TcpListener::bind(&addr, &handle).unwrap();
+
+    // Once we've got the TCP listener, inform that we have it
+    println!("Listening on: {}", addr);
+
+    // Pull out the stream of incoming connections and then for each new
+    // one spin up a new task copying data.
+    //
+    // We use the `io::copy` future to copy all data from the
+    // reading half onto the writing half.
+    let done = socket.incoming().for_each(move |(mut conn, _addr)| {
+        let lazy = futures::lazy(move || {
+            let fib = tokio_fiber::Fiber::new(move || -> io::Result<()> {
+                let mut buf = [0u8; 1024 * 64];
+                loop {
+                    let size = poll!(conn.read(&mut buf))?;
+                    if size == 0 {/* eof */ break; }
+                    let _ = poll!(conn.write_all(&mut buf[0..size]))?;
+                }
+
+                Ok(())
+            });
+
+            fib.map_err(|e| {
+                println!("error: {}", e);
+            })
+        });
+
+        handle.spawn(lazy);
+
+        Ok(())
+    });
+    l.run(done).unwrap();
+}
---
+++ src/lib.rs
@@ -0,0 +1,135 @@
+extern crate fringe;
+extern crate tokio_core;
+#[macro_use(task_local)]
+extern crate futures;
+
+use futures::Async;
+
+use std::cell::{RefCell, UnsafeCell};
+
+
+/// Convenient macro to suspend and retry async operation
+/// just as it was blocking operation
+#[macro_export]
+macro_rules! poll {
+    ($e:expr) => {{
+        let res;
+        loop {
+             match $e {
+                Err(ref e) if e.kind() == ::std::io::ErrorKind::WouldBlock => {
+                    $crate::yield_now();
+                }
+                other => {
+                    res = Some(other);
+                    break
+                },
+            }
+        }
+        res.unwrap()
+        }}
+}
+
+task_local! {
+    static TL_YIELDER : RefCell<Vec<YielderRefStore>> = RefCell::new(vec!())
+}
+
+// Unsafe magic to for task-local yidler
+//
+// TODO: Need wiser people to verify this not not buggy
+//
+// `fringe` gives coroutine `&'a mut Yielder<I, O>`
+//
+// to have access to it from `yield_now`, use
+// an escape hatch and be extremely careful
+struct YielderRefStore(UnsafeCell<&'static Yield>);
+
+impl YielderRefStore {
+    unsafe fn new<'a>(y : &'a Yield) -> YielderRefStore {
+        YielderRefStore (
+             UnsafeCell::new(std::mem::transmute(y))
+        )
+    }
+}
+
+unsafe impl Send for YielderRefStore {}
+
+// Wrapper over `fringe::Yielder` to make put it in `task_local`
+trait Yield {
+    fn suspend(&self);
+}
+
+impl<I : Send, O : Send, E : Send> Yield for fringe::generator::Yielder<I, SuspendCommand<O, E>> {
+    fn suspend(&self) {
+        let _ = self.suspend(Ok(Async::NotReady));
+    }
+}
+// We wake up coroutine passing this to it's inside
+enum ResumeCommand {
+    Unblock
+}
+
+// We suspend coroutine passing this to the outside
+pub type SuspendCommand<O, E> = futures::Poll<O, E>;
+
+unsafe fn yielder_tl_push(y : &Yield) {
+    TL_YIELDER.with(|tl_yielder| {
+        let mut tl_yielder = tl_yielder.borrow_mut();
+        tl_yielder.push(YielderRefStore::new(y));
+    });
+}
+
+unsafe fn yielder_tl_pop() -> &'static Yield {
+    let yielder : YielderRefStore  = TL_YIELDER.with(|tl_yielder| {
+        let mut tl_yielder = tl_yielder.borrow_mut();
+        tl_yielder.pop().unwrap()
+    });
+
+    yielder.0.into_inner()
+}
+
+pub fn yield_now() {
+    let y = unsafe { yielder_tl_pop() };
+
+    y.suspend();
+
+    unsafe { yielder_tl_push(y) }
+}
+
+pub struct Fiber<'a, O : Send+'a, E : Send+'a> {
+    co : fringe::Generator<'a, ResumeCommand, SuspendCommand<O, E>, fringe::OsStack>,
+}
+
+impl<'a, O : Send, E : Send> Fiber<'a, O, E> {
+    pub fn new<F>(f : F) -> Self
+        where
+        F: FnOnce() -> Result<O, E> + Send +'a {
+            let stack = fringe::OsStack::new(1 << 20).unwrap();
+            let gen = fringe::Generator::new(stack, move |yielder, _resume_cmd : ResumeCommand| {
+                unsafe { yielder_tl_push(yielder) };
+
+                let res = f();
+
+                // pop the `Yield`, but use the "typed" version
+                let _pop_and_discard = unsafe { yielder_tl_pop() };
+                // TODO: can any optimizations prevent `yielder` to see
+                // changes introduced when task-local version was used?
+
+                yielder.suspend(match res {
+                    Ok(t) => Ok(Async::Ready(t)),
+                    Err(e) => Err(e)
+                });
+            });
+
+            Fiber {
+                co : gen
+            }
+         }
+}
+
+impl<'a, O : Send, E : Send> futures::Future for Fiber<'a, O, E> {
+    type Item = O;
+    type Error = E;
+    fn poll(&mut self) -> futures::Poll<Self::Item, Self::Error> {
+        self.co.resume(ResumeCommand::Unblock).expect("poll on finished Fiber is illegal")
+    }
+}
